6db92488eca6ff9d10b80ec2bd9ef241ed5c9d7a
Pogen <76914800+IPogenI@users.noreply.github.com>
2024-07-09 18:00:09 +0600

    Fixing Story Repeation

--- src/pages/NewsFeedPosts.jsx
+++ src/pages/NewsFeedPosts.jsx
@@ -12,6 +12,86 @@ const NewsFeedPosts = () => {
             <div className="posts pt-24 flex flex-col bg-gray-100">
                 <div className="flex justify-center flex-none">
                     <section className="flex flex-col max-w-xl gap-5">
+                        {/* Story/Day of Friends */}
+                        <div className="stories py-4">
+                            <div className="bg-white border-2 border-gray-300 rounded-lg ">
+                                <div className="flex py-2 justify-between px-4">
+
+                                    <div className="text-center">
+                                        <div className="rounded-full p-0.5 bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500">
+                                            <div className="h-14 w-14 rounded-full bg-white wrapper overflow-hidden border-2 border-black">
+                                                <img className="w-full h-full object-contain" src="https://randomuser.me/api/portraits/men/1.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                        <p className="text-black text-xs pb-2 pt-1">John Doe</p>
+                                    </div>
+
+                                    <div className="text-center">
+                                        <div className="rounded-full p-0.5 bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500">
+                                            <div className="h-14 w-14 rounded-full bg-white wrapper overflow-hidden border-2 border-black">
+                                                <img className="w-full h-full object-contain" src="https://randomuser.me/api/portraits/men/2.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                        <p className="text-black text-xs pb-2 pt-1">John Doe</p>
+                                    </div>
+
+                                    <div className="text-center">
+                                        <div className="rounded-full p-0.5 bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500">
+                                            <div className="h-14 w-14 rounded-full bg-white wrapper overflow-hidden border-2 border-black">
+                                                <img className="w-full h-full object-contain" src="https://randomuser.me/api/portraits/men/3.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                        <p className="text-black text-xs pb-2 pt-1">John Doe</p>
+                                    </div>
+
+                                    <div className="text-center">
+                                        <div className="rounded-full p-0.5 bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500">
+                                            <div className="h-14 w-14 rounded-full bg-white wrapper overflow-hidden border-2 border-black">
+                                                <img className="w-full h-full object-contain" src="https://randomuser.me/api/portraits/men/4.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                        <p className="text-black text-xs pb-2 pt-1">John Doe</p>
+                                    </div>
+
+                                    <div className="text-center">
+                                        <div className="rounded-full p-0.5 bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500">
+                                            <div className="h-14 w-14 rounded-full bg-white wrapper overflow-hidden border-2 border-black">
+                                                <img className="w-full h-full object-contain" src="https://randomuser.me/api/portraits/men/5.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                        <p className="text-black text-xs pb-2 pt-1">John Doe</p>
+                                    </div>
+
+                                    <div className="text-center">
+                                        <div className="rounded-full p-0.5 bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500">
+                                            <div className="h-14 w-14 rounded-full bg-white wrapper overflow-hidden border-2 border-black">
+                                                <img className="w-full h-full object-contain" src="https://randomuser.me/api/portraits/men/6.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                        <p className="text-black text-xs pb-2 pt-1">John Doe</p>
+                                    </div>
+
+                                    <div className="text-center">
+                                        <div className="rounded-full p-0.5 bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500">
+                                            <div className="h-14 w-14 rounded-full bg-white wrapper overflow-hidden border-2 border-black">
+                                                <img className="w-full h-full object-contain" src="https://randomuser.me/api/portraits/men/7.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                        <p className="text-black text-xs pb-2 pt-1">John Doe</p>
+                                    </div>
+
+                                    <div className="text-center">
+                                        <div className="rounded-full p-0.5 bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500">
+                                            <div className="h-14 w-14 rounded-full bg-white wrapper overflow-hidden border-2 border-black">
+                                                <img className="w-full h-full object-contain" src="https://randomuser.me/api/portraits/men/8.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                        <p className="text-black text-xs pb-2 pt-1">John Doe</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
 
                         {/* Iterating over data to create posts */}
                         {
