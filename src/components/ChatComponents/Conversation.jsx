7231e6d9e05e926cc7fa3e75136b677a17d87dcc
Pogen <76914800+IPogenI@users.noreply.github.com>
2024-08-17 14:44:58 +0600

    Conversation name fix

--- src/components/ChatComponents/Conversation.jsx
+++ src/components/ChatComponents/Conversation.jsx
@@ -3,12 +3,12 @@ import { AuthContext } from '../../contexts/AuthContext/AuthContext'
 
 const Conversation = ({conv, onClick}) => {
     let {photo, conversationName, users, isGroup} = conv
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
 
     //implement conversation name function if not given
-    // if(!conversationName) {
-    //     conversationName = 
-    // }
+    if(!conversationName) {
+        conversationName = "Default"
+    }
     return (
         <div className="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md" onClick={() => {onClick(conv)}}>
             <div className="w-12 h-12 bg-gray-300 rounded-full mr-3">
